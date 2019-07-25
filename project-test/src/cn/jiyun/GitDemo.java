b9f3a643e7a32a7075329709ce13976956fcaf60
roy <1665616225@qq.com>
2019-07-25 17:41:51 +0800

    一个java工程，同步到github测试；

---
+++ project-test/src/cn/jiyun/GitDemo.java
@@ -0,0 +1,12 @@
+package cn.jiyun;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		int a =3;
+		show();
+	}
+
+	public static void show() {
+		System.out.println("我就是测试一下git");
+	}
+}
